1359412d23fe503a59e49b3f9405b338ba4ca6bf
JieLiu <1538757925@qq.com>
2020-03-08 23:42:13 +0800

    支付模块构建

---
+++ cloud-provider-payment8001/src/main/java/com/atlj/springcloud/PaymentMain8001.java
@@ -0,0 +1,11 @@
+package com.atlj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class);
+    }
+}
