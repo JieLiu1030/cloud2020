13a394419e4343b1f31108c5307a7d983bfe34ae
JieLiu <1538757925@qq.com>
2020-03-12 00:24:31 +0800

    集群部署

--- cloud-eureka-server7002/src/main/java/com/atlj/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/atlj/springcloud/EurekaMain7002.java
@@ -2,13 +2,14 @@ package com.atlj.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7002 {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+
+        SpringApplication.run(EurekaMain7002.class,args);
     }
 }
