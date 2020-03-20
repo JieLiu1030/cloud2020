44ad254d1e04009b532ea03d67311b972d462ee3
JieLiu <1538757925@qq.com>
2020-03-20 16:07:51 +0800

    springcloud bus消息总线 + rabbitmq

---
+++ cloud-config-client-3366/src/main/java/com/atlj/springcloud/ClientConfigMain3366.java
@@ -0,0 +1,14 @@
+package com.atlj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ClientConfigMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientConfigMain3366.class,args);
+    }
+}
